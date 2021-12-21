c56fb75af585be27154784e2ae6aba4816720e69
PierreEdFils <epierrefils@gmail.com>
2021-12-20 19:37:30 -0500

    Add alg1 & 2

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -4,9 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(getFirstElement(new int[] {3,1,4}));
+       // System.out.println(getFirstElement(new int[] {10,1,4}));
+        System.out.println(addition(3));
     }
+
+    //Alg 1
     public static int getFirstElement(int[] arr){
+
         return arr[0];
+    }
+
+    //Alg 2
+    public static int addition(int num) {
+        return ++num;
     }
 }
